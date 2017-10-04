da1912bcc0cb9a0ac31b324f5c36ac6f6d28c048
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-10-05 00:57:00 +0100

    stating proof of concept

--- src/uk/co/rodderscode/bbc/Fetcher.java
+++ src/uk/co/rodderscode/bbc/Fetcher.java
@@ -1,35 +1,20 @@
 package uk.co.rodderscode.bbc;
 
-import java.net.*;
-import java.io.*;
-
 
 public class Fetcher {
 
     public static void main(String[] args)
     {
-        Fetcher f = new Fetcher();
-        System.out.println("hey");
+
+        // start scanner loop
+        // split into list
+        // fetch urls
+        // capture data
+        // make calculations
+        // compose maps
+        // output json
+
     }
-
-
-    public void reader() throws Exception
-    {
-        URL yahoo = new URL("http://www.yahoo.com/");
-
-        URLConnection yc = yahoo.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(yc.getInputStream())
-        );
-
-        String inputLine;
-
-        while ((inputLine = bufferedReader.readLine()) != null)
-            System.out.println(inputLine);
-
-        bufferedReader.close();
-    }
-
 
 
 }
