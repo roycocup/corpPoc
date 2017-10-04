8b058c74a1c3949418089d3eda7841e436af5dc0
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-10-05 00:43:44 +0100

    moving exploratory code on to branches

--- src/uk/co/rodderscode/bbc/Fetcher.java
+++ src/uk/co/rodderscode/bbc/Fetcher.java
@@ -8,28 +8,8 @@ public class Fetcher {
 
     public static void main(String[] args)
     {
-        Fetcher f = new Fetcher();
         System.out.println("hey");
     }
-
-
-    public void reader() throws Exception
-    {
-        URL yahoo = new URL("http://www.yahoo.com/");
-
-        URLConnection yc = yahoo.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(yc.getInputStream())
-        );
-
-        String inputLine;
-
-        while ((inputLine = bufferedReader.readLine()) != null)
-            System.out.println(inputLine);
-
-        bufferedReader.close();
-    }
-
 
 
 }
