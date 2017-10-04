7df9a7bae3775af28f7b9827b72dede5e5bbb539
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-10-05 00:42:33 +0100

    exploration

--- src/uk/co/rodderscode/bbc/Fetcher.java
+++ src/uk/co/rodderscode/bbc/Fetcher.java
@@ -6,28 +6,55 @@ import java.io.*;
 
 public class Fetcher {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Fetcher f = new Fetcher();
-        System.out.println("hey");
+
+
+        String d = f.getTestData();
+
+        try {
+            f.fetch(new URL("http://yahoo.com"));
+        } catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public String getTestData()
+    {
+        return "http://www.bbc.co.uk/iplayer\n" +
+                "https://google.com\n" +
+                "bad://address\n" +
+                "http://www.bbc.co.uk/missing/thing\n" +
+                "http://not.exists.bbc.co.uk/\n" +
+                "http://www.oracle.com/technetwork/java/javase/downloads/index.html https://www.pets4homes.co.uk/images" +
+                        "/articles/1646/large/kitten-emergencies-signs-to-look-out-for- 537479947ec1c.jpg\n" +
+                "http://site.mockito.org/" +
+                "\n\n";
     }
 
 
-    public void reader() throws Exception
-    {
-        URL yahoo = new URL("http://www.yahoo.com/");
+    public void fetch(URL url) {
 
-        URLConnection yc = yahoo.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(yc.getInputStream())
-        );
+        BufferedReader bufferedReader;
 
-        String inputLine;
+        try{
+            URLConnection yc = url.openConnection();
+            bufferedReader = new BufferedReader(
+                    new InputStreamReader(yc.getInputStream())
+            );
+            String inputLine;
 
-        while ((inputLine = bufferedReader.readLine()) != null)
-            System.out.println(inputLine);
+            while ((inputLine = bufferedReader.readLine()) != null)
+                System.out.println(inputLine);
 
-        bufferedReader.close();
+            bufferedReader.close();
+        } catch (IOException e){
+            System.out.println("Problems opening connection");
+        }
+
+
+
     }
 
 
