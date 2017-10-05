7aad9e5cd193adba4e36afe111f341002f8b3e8f
Rodrigo Dias <rodrigo@rodderscode.co.uk>
2017-10-05 10:18:35 +0100

    fixing gradle run + poc init

--- src/uk/co/rodderscode/bbc/Fetcher.java
+++ src/uk/co/rodderscode/bbc/Fetcher.java
@@ -1,12 +1,23 @@
 package uk.co.rodderscode.bbc;
 
 
+import java.util.Scanner;
+
 public class Fetcher {
 
     public static void main(String[] args)
     {
 
         // start scanner loop
+        Scanner input = new Scanner( System.in );
+        System.out.println("Enter urls followed by newline. A single newline will mark EOF");
+
+        String text;
+
+        text = input.next();
+
+        System.out.println("you said: " + text);
+
         // split into list
         // fetch urls
         // capture data
